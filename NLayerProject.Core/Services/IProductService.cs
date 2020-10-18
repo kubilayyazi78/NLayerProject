7a283d234ad9cdc72bc6721cd7840a6b2a95128c
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-10-18 22:10:27 +0300

    IProductService oluşturuldu.

---
+++ NLayerProject.Core/Services/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using NLayerProject.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Core.Services
+{
+    interface IProductService:IService<Product>
+    {
+        Task<Product> GetWithCategoryByIdAsync(int productId); //veritabanı ile ilgili işlem
+        // bool ControlInnerBarcode(Product product); iç metot veritabanı işlemi değil kontrol işlemi
+    }
+}
