1292b3ad28f34f55f2598a85ce365b3c0295a9db
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-10-25 01:19:51 +0300

    CategorySeed sınıfı oluşturuldu.

--- NLayerProject.Data/AppDbContext.cs
+++ NLayerProject.Data/AppDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NLayerProject.Core.Models;
 using NLayerProject.Data.Configurations;
+using NLayerProject.Data.Seeds;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -22,6 +23,10 @@ namespace NLayerProject.Data
         {
             modelBuilder.ApplyConfiguration(new ProductConfiguration());
             modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+
+
+            modelBuilder.ApplyConfiguration(new ProductSeed(new int[] { 1,2}));
+            modelBuilder.ApplyConfiguration(new CategorySeed(new int[] { 1,2}));
         }
     }
 }
