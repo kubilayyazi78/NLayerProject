532c3f3c83cebfdd5a9e8314b77f724002570080
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-10-26 22:37:02 +0300

    Categorycontroller oluşturuldu.

---
+++ NLayerProject.API/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayerProject.Core.Services;
+
+namespace NLayerProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+
+            return Ok(categories);
+        }
+    }
+}
