ef82f795a6158b445466d23444e4dbc9419b7ab4
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-10-27 22:45:15 +0300

    Update işlemi tamamlandı.

--- NLayerProject.API/Controllers/CategoryController.cs
+++ NLayerProject.API/Controllers/CategoryController.cs
@@ -48,5 +48,15 @@ namespace NLayerProject.API.Controllers
             return Created(string.Empty, _mapper.Map<CategoryDto>(newCategory));
               
         }
+
+        [HttpPut]
+        public IActionResult Update (CategoryDto categoryDto)
+        {
+            var category = _categoryService.Update(_mapper.Map<Category>(categoryDto));
+
+            return NoContent();
+
+            
+        }
     }
 }
