26bad3fa20ed27c011a21bfaf0ce3b8721efc1d2
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-10-29 01:05:23 +0300

    Startup sınıfı sadeleştirildi.

--- NLayerProject.API/Startup.cs
+++ NLayerProject.API/Startup.cs
@@ -16,6 +16,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using NLayerProject.API.DTOs;
+using NLayerProject.API.Extensions;
 using NLayerProject.API.Filters;
 using NLayerProject.Core.Repositories;
 using NLayerProject.Core.Services;
@@ -79,32 +80,7 @@ namespace NLayerProject.API
                 app.UseDeveloperExceptionPage();
             }
 
-
-            app.UseExceptionHandler(config =>
-            {
-                config.Run(async context =>
-                {
-                    context.Response.StatusCode = 500;
-                    context.Response.ContentType = "application/json";
-                    var error = context.Features.Get<IExceptionHandlerFeature>();
-
-                    if (error!=null)
-                    {
-                        var ex = error.Error;
-
-                        ErrorDto errorDto = new ErrorDto();
-
-                        errorDto.Status = 500;
-                        errorDto.Errors.Add(ex.Message);
-
-                        await context.Response.WriteAsync(JsonConvert.SerializeObject(errorDto));
-                    }
-
-                });
-
-            });
-
-
+            app.UseCustomException();
 
             app.UseHttpsRedirection();
 
