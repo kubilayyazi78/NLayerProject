890af1a4e28c88c9dbcaeba2bdef246ced685c5e
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-10-28 18:00:40 +0300

    Personcontroller oluşturuldu.

---
+++ NLayerProject.API/Controllers/PersonsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayerProject.Core.Models;
+using NLayerProject.Core.Services;
+
+namespace NLayerProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly IService<Person> _service;
+        private readonly IMapper _mapper;
+
+        public PersonsController(IService<Person> service,IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var persons = await _service.GetAllAsync();
+            return Ok(persons);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(Person person)
+        {
+            var newPerson=  await _service.AddAsync(person);
+
+            return Created(string.Empty, newPerson);
+
+        }
+
+    }
+}
