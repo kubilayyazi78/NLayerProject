546c04ec2fbb98b39778a457280f3c4dea486861
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-10-30 00:47:50 +0300

    Categorycontroller oluşturuldu.

---
+++ NLayerProject.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NLayerProject.Core.Services;
+
+namespace NLayerProject.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public CategoriesController(ICategoryService categoryService,IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
