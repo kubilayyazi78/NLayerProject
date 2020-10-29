d9cd0f48e39529484bc73c90df41622a1b4b2e94
kubilayyazi78 <kubilayyazi@hotmail.com>
2020-10-29 18:09:27 +0300

    Web startup sınıfı güncellendi.

--- NLayerProject.Web/Startup.cs
+++ NLayerProject.Web/Startup.cs
@@ -5,9 +5,17 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NLayerProject.Core.Repositories;
+using NLayerProject.Core.Services;
+using NLayerProject.Core.UnitOfWorks;
+using NLayerProject.Data;
+using NLayerProject.Data.Repositories;
+using NLayerProject.Data.UnitOfWorks;
+using NLayerProject.Service.Services;
 
 namespace NLayerProject.Web
 {
@@ -23,6 +31,26 @@ namespace NLayerProject.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped(typeof(IService<>), typeof(Service<>));
+
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+
+                options.UseSqlServer(Configuration["ConnectionStrings:SqlConStr"].ToString(), o =>
+                {
+                    o.MigrationsAssembly("NLayerProject.Data");
+                });
+
+            });
+
+
             services.AddControllersWithViews();
         }
 
